9a2598937adaf4068a4446cc35d3d6a643bc5d79
nikitasemenchenko <semenchenko_06@mail.ru>
2025-07-09 18:40:30 +0500

    Добавлена функция генерации следующего поколения с проверкой на правила игры, добавлена функция запуска игры. Теперь в консоли полностью рабочая версия игрф Жизнь. Предстоит добавить необходимые библиотеки, и создать интерфейс: Окно с игрой, кнопки.

--- Life.h
+++ Life.h
@@ -2,24 +2,25 @@
 #define LIFE_H
 
 #include <vector>
-#include <iostream>
-
-//размеры поля
-const int X = 15; 
-const int Y = 15;
 
 class Life {
 private:
     std::vector<std::vector<bool>> life; //само пространство с клетками
+    //размеры поля
+    const int X = 25; 
+    const int Y = 25;
+    //скорость игры в миллисекундах
+    const int speed = 500;
+
     int getNeighbours(int x, int y);
+    void print();
+    void randomFill();
+    void nextGen();
     
 public:
     Life();
     ~Life() = default; // деструктор пустой, так как использую vector и он освобождает память сам
-    
-    void print();
-    void randomFill();
-    void nextGen();
+
     void generateLife();
 };
 
