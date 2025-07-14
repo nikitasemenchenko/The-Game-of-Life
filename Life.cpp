d27f041a0bad82555e39cdf93f2665ccdd465bba
nikitasemenchenko <semenchenko_06@mail.ru>
2025-07-14 20:55:52 +0500

    Добавлена кнопка очистки поля, добавлен режим рисования клеток. Улучшена структура

--- Life.cpp
+++ Life.cpp
@@ -26,7 +26,7 @@ void Life::print(){
         for(int j = 0; j < X; ++j){
             Color color = life[i][j] ? GREEN : BLACK; // живая клетка - зелёная, мёртвая - чёрная
 
-            Rectangle cell = { static_cast<float>(j * cellSize), static_cast<float>(i * cellSize),
+            Rectangle cell = { static_cast<float>(j * cellSize + panelWidth), static_cast<float>(i * cellSize),
                 static_cast<float>(cellSize - 1), static_cast<float>(cellSize -1) }; // -1 чтобы было видно сетку
 
             DrawRectangleRec(cell, color); // рисуем клетку
@@ -59,93 +59,88 @@ void Life::randomFill(){
     }
 }
 
-void Life::generateLife() {
-    const int panelWidth = 220; //ширина панели управления
-    
-    InitWindow(width + panelWidth, height, "The Game of Life");
-    SetTargetFPS(60);
-
-    //скорость
-    const int speedLevels[5] = {1, 5, 10, 20, 30};
-    int currentSpeedIndex = 2; //начальная скорость 10 fps
-    float simulationTimer = 0.0f;
-    bool isPaused = true; //начало с паузы
-
-    for (auto& row : life) {
-        std::fill(row.begin(), row.end(), false); //от начала до конца заполняем "мёртным" значением
+void Life::clear() {
+    for(int i = 0; i < Y; ++i){
+        for(int j = 0; j < X; ++j){
+            life[i][j] = false;
+        }
     }
+} 
+void Life::generateButton() {
+    Rectangle generateBtn = {20, 220, 180, 40};
+    bool generateHover = CheckCollisionPointRec(GetMousePosition(), generateBtn);
+    DrawRectangleRec(generateBtn, generateHover ? LIGHTGRAY : GRAY); 
 
-    while (!WindowShouldClose()) {
-        float deltaTime = GetFrameTime(); //замер времени между кадрами
-        
-        //рабочие кнопки
-        if (IsKeyPressed(KEY_UP)) currentSpeedIndex = std::min(4, currentSpeedIndex + 1);
-        if (IsKeyPressed(KEY_DOWN)) currentSpeedIndex = std::max(0, currentSpeedIndex - 1);
-        if (IsKeyPressed(KEY_SPACE)) isPaused = !isPaused;
+    //центрирование GENERATE
+    const char* generateText = "GENERATE";
+    int textWidth = MeasureText(generateText, 20);
+    int textX = generateBtn.x + (generateBtn.width - textWidth) / 2;
+    int textY = generateBtn.y + (generateBtn.height - 20) / 2;
+    DrawText(generateText, textX, textY, 20, BLACK);
 
-        //обновление поля
-        if (!isPaused) {
-            simulationTimer += deltaTime;
-            float updateInterval = 1.0f / speedLevels[currentSpeedIndex];
-            while (simulationTimer >= updateInterval) { //накопили нужный временной интервал - обновляем игру
-                simulationTimer -= updateInterval;
-                nextGen();
-            }
-        }
-
-        //визуал
-        BeginDrawing();
-
-        ClearBackground(GRAY);
-        DrawRectangle(0, 0, panelWidth, height, DARKGRAY); //панель управления
-        DrawRectangle(panelWidth, 0, width, height, BLACK);  //игровое поле
-        // print();
-        
-        //отрисовка клеток с учетом смещения (из-за панели)
-        for (int row = 0; row < Y; ++row) {
-        for (int col = 0; col < X; ++col) {
-        //вычисление координаты клетки
-        const float posX = panelWidth + col * cellSize;
-        const float posY = row * cellSize;
-        
-        //клетка с границами
-        DrawRectangle(posX, posY, cellSize-1, cellSize-1, life[row][col] ? GREEN : BLACK);
-            }
-        }
-
-        //текст для навигации
-        DrawText("CONTROLS", 20, 20, 20, WHITE);
-        DrawText("SPACE: Pause/Resume", 20, 50, 15, WHITE);
-        DrawText("UP/DOWN: Change speed", 20, 80, 15, WHITE);
-            
-        DrawText(TextFormat("Speed: %d FPS", speedLevels[currentSpeedIndex]), 20, 120, 20, WHITE);
-        DrawText(isPaused ? "[PAUSED]" : "[RUNNING]", 20, 150, 20, isPaused ? RED : GREEN);
-        
-        //визуальный выбор скорости
-        for (int i = 0; i < 5; i++) { 
-            Color col = (i <= currentSpeedIndex) ? GREEN : DARKGREEN;
-            DrawRectangle(20 + i*30, 180, 25, 25, col);
-            }
-
-        //кнопка Generate
-        Rectangle generateBtn = {20, 220, 180, 40};
-        bool generateHover = CheckCollisionPointRec(GetMousePosition(), generateBtn);
-        DrawRectangleRec(generateBtn, generateHover ? LIGHTGRAY : GRAY); 
-
-            //центрирование GENERATE
-        const char* generateText = "GENERATE";
-        int textWidth = MeasureText(generateText, 20);
-        int textX = generateBtn.x + (generateBtn.width - textWidth) / 2;
-        int textY = generateBtn.y + (generateBtn.height - 20) / 2;
-        DrawText(generateText, textX, textY, 20, BLACK);
-
+    //обработка клика
+    if (generateHover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && isPaused) {
+        randomFill();
+    }
+}
+void Life::clearButton() {
+    Rectangle clearBtn = {20, 270, 180, 40};
+        bool clearHover = CheckCollisionPointRec(GetMousePosition(), clearBtn);
+        DrawRectangleRec(clearBtn, clearHover ? LIGHTGRAY : GRAY);
+        //центрирование названия кнопки
+        const char* clearText = "CLEAR";
+        int textWidth = MeasureText(clearText, 20);
+        int textX = clearBtn.x + (clearBtn.width - textWidth) / 2;
+        int textY = clearBtn.y + (clearBtn.height - 20) / 2;
+        DrawText(clearText, textX, textY, 20, BLACK);
         //обработка клика
-        if (generateHover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            randomFill();
+        if (clearHover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && isPaused) {
+            clear(); // очищаем поле
         }
-        EndDrawing();
+}
+
+void Life::information(){
+    DrawText("CONTROLS", 20, 20, 20, WHITE);
+    DrawText("SPACE: Pause/Resume", 20, 50, 15, WHITE);
+    DrawText("UP/DOWN: Change speed", 20, 80, 15, WHITE);      
+    DrawText(TextFormat("Speed: %d FPS", speedLevels[currentSpeedIndex]), 20, 120, 20, WHITE);
+    DrawText(isPaused ? "[PAUSED]" : "[RUNNING]", 20, 150, 20, isPaused ? RED : GREEN);
+}
+
+void Life::draw() {
+    if (!drawingMode || !isPaused) return;
+    Vector2 mousePos = GetMousePosition();
+    int posX = (mousePos.x - panelWidth) / cellSize;
+    int posY = mousePos.y / cellSize;
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && isPaused && posX >= 0 && posX < X && posY >= 0 && posY < Y) {
+        life[posY][posX] = !life[posY][posX];
     }
-    CloseWindow();
+}
+
+void Life::drawButtonClick() {
+    drawingMode = !drawingMode;
+}
+
+void Life::drawButton() {
+    Rectangle drawButton = {20, 320, 180, 40};
+    bool drawHover = CheckCollisionPointRec(GetMousePosition(), drawButton);
+    Color buttonColor;
+    if(drawingMode) {
+        buttonColor = drawHover ? GREEN : LIME;
+    } else {
+        buttonColor = drawHover ? LIGHTGRAY : GRAY;
+    }
+    DrawRectangleRec(drawButton, buttonColor);
+    //центрирование названия кнопки
+    const char* text = "DRAW";
+    int textWidth = MeasureText(text, 20);
+    int textX = drawButton.x + (drawButton.width - textWidth) / 2;
+    int textY = drawButton.y + (drawButton.height - 20) / 2;
+    DrawText(text, textX, textY, 20, BLACK);
+
+    if (drawHover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && isPaused) {
+        drawButtonClick();
+    }
 }
 
 void Life::nextGen() {
@@ -167,4 +162,56 @@ void Life::nextGen() {
         }
     }
     life = newLife; // обновляем состояние клеток
+}
+
+
+void Life::generateLife() {  
+    InitWindow(width + panelWidth, height, "The Game of Life");
+
+    float simulationTimer = 0.0f;
+
+    while (!WindowShouldClose()) {
+        float deltaTime = GetFrameTime(); //замер времени между кадрами
+
+        //обновление поля
+        if (!isPaused) {
+            simulationTimer += deltaTime;
+            float updateInterval = 1.0f / speedLevels[currentSpeedIndex];
+            while (simulationTimer >= updateInterval) { //накопили нужный временной интервал - обновляем игру
+                simulationTimer -= updateInterval;
+                nextGen();
+            }
+        }
+
+        //Горячие клавиши
+        if (IsKeyPressed(KEY_UP)) currentSpeedIndex = std::min(4, currentSpeedIndex + 1);
+        if (IsKeyPressed(KEY_DOWN)) currentSpeedIndex = std::max(0, currentSpeedIndex - 1);
+        if (IsKeyPressed(KEY_SPACE)) isPaused = !isPaused;
+
+        //визуал
+        BeginDrawing();
+
+        ClearBackground(GRAY);
+        DrawRectangle(0, 0, panelWidth, height, DARKGRAY); //панель управления
+        DrawRectangle(panelWidth, 0, width, height, DARKGRAY);  //игровое поле
+        
+        print(); // отрисовка клеток
+
+        information();
+        
+        //визуальный выбор скорости
+        for (int i = 0; i < 5; i++) { 
+            Color col = (i <= currentSpeedIndex) ? GREEN : DARKGREEN;
+            DrawRectangle(20 + i*30, 180, 25, 25, col);
+            }
+
+        generateButton();
+        clearButton();
+        drawButton();
+
+        if (drawingMode) draw();
+
+        EndDrawing();
+    }
+    CloseWindow();
 }
--- Life.h
+++ Life.h
@@ -7,24 +7,30 @@ class Life {
 private:
     std::vector<std::vector<bool>> life; //само пространство с клетками
     //размеры поля
-    const int width = 600; 
-    const int height = 600;
+    const int width = 800; 
+    const int height = 800;
 
     const int cellSize = 20; //размер клетки
     const int X = width / cellSize; //количество клеток по горизонтали
     const int Y = height / cellSize; //количество клеток по вертикали
 
-    //скорость игры
-    // const int fps = 2;
-    int fps;
-    bool isPaused;
-    float sliderValue;
+    const int panelWidth = 220; //ширина панели управления
+    bool isPaused = true;
+    const int speedLevels[5] = {1, 5, 10, 20, 30};
+    int currentSpeedIndex = 2; //начальная скорость 10 fps
+    bool drawingMode = false;
 
     int getNeighbours(int x, int y);
     void print();
     void randomFill();
     void nextGen();
-    
+    void clear();
+    void generateButton();
+    void clearButton();
+    void information();
+    void draw();
+    void drawButton();
+    void drawButtonClick();
 public:
     Life();
     ~Life() = default; // деструктор пустой, так как использую vector и он освобождает память сам
